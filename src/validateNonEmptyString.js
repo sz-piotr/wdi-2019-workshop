293624b1d4092a18d62f55822f6bd3a8083320ce
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-03-25 19:59:41 +0100

    Add validateNonEmptyString tests

--- src/validateNonEmptyString.js
+++ src/validateNonEmptyString.js
@@ -1,3 +1,7 @@
+import { validateAll } from './validateAll'
+import { validateString } from './validateString'
+import { validateNot } from './validateNot'
+
 export const validateNonEmptyString = validateAll(
   validateString,
   validateNot('')
---
+++ test/validateNonEmptyString.test.js
@@ -0,0 +1,30 @@
+import { validateNonEmptyString } from '../src/validateNonEmptyString'
+
+describe('validateNonEmptyString', () => {
+  it('succeds for strings', () => {
+    expect(validateNonEmptyString('Hello World!')).toEqual([])
+  })
+
+  it('fails for empty strings', () => {
+    expect(validateNonEmptyString('')).toEqual([
+      { path: '', expected: 'not ""' }
+    ])
+  })
+
+  it.each([
+    [1],
+    [true],
+    [{ x: 1, y: 2 }],
+    [[1, 'foo']],
+  ])('fails for %s', (value) => {
+    expect(validateNonEmptyString(value)).toEqual([
+      { path: '', expected: 'string' }
+    ])
+  })
+
+  it('respects the provided path', () => {
+    expect(validateNonEmptyString(0, 'x.y')).toEqual([
+      { path: 'x.y', expected: 'string' }
+    ])
+  })
+})
